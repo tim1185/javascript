d6f01546620f00c0df0c7ac0e646e842d82687b9
dannenmann tim <dannenmanntim@hotmail.com>
2020-06-08 16:40:50 +0200

    chapter one, complete

---
+++ 01-base/03-prompt-if/script.js
@@ -0,0 +1,19 @@
+/* becode/javascript
+ *
+ * /01-base/03-prompt-if/script.js - 1.3: promp & if
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+var person = prompt ('hello, do you wanna have some cake');
+
+if (person == 'yes'){
+  alert('congratz');
+}
+else{
+  alert('more cake for me');
+}
---
+++ 01-base/04-asv-confirm/script.js
@@ -0,0 +1,18 @@
+/* becode/javascript
+ *
+ * /01-base/04-asv-confirm/script.js - 1.4: ASV avec confirmation
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+function dialog() {
+    var age = prompt("what is your age?");
+    var gender = prompt("what is your gender?");
+    var location = prompt("what city do you live?");
+    alert ("your age is: " + age + " you are? " + gender + " you live in: " + location);
+}
+dialog()
